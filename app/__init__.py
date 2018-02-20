d1085da802d28131cf1468a7a5b595a89a6b563f
Collins njau <collinsnjau39@gmail.com>
2018-02-20 17:41:19 +0300

    I have added the flask-login library and initialized the login manager in the __init__ file and added the login_message and login_view where to redirect users who are not authenticated

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 #third party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 #LOCAL IMPORT
 from config import app_config
@@ -9,6 +10,9 @@ from config import app_config
 #initialize the db
 db=SQLAlchemy()
 
+#inittilize the loginManager object
+login_manager=LoginManager()
+
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -16,8 +20,9 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    @app.route('/')
-    def hello():
-        return 'Hello'
+    login_manager.init_app(app)
+    login_manager.login_message="YOU MUST BE LOGGED IN"
+    login_manager.login_view="auth.login"
 
     return app
+    
