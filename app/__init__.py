c04c2769d8bbcb25ca27ff16fc350bc159e1269b
Collins njau <collinsnjau39@gmail.com>
2018-02-22 02:16:27 +0300

    I have added the bootstrap library and initialized it in the init file

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 #LOCAL IMPORT
 from config import app_config
@@ -20,6 +21,7 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    Bootstrap(app)
 
     login_manager.init_app(app)
     login_manager.login_message="YOU MUST BE LOGGED IN"
