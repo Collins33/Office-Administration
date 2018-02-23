93d3ec62b67a5e204fbf56b8595c19fa0a7886b2
Collins njau <collinsnjau39@gmail.com>
2018-02-23 14:22:19 +0300

    I have added the class to create the department form for the admin to add and edit a department

---
+++ app/admin/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
+#create the department form as a class that inherits from FlaskForm
+class DepartmentForm(FlaskForm):
+    #for the admin to add or edit a department
+    name=StringField('Name',validators=[DataRequired()])
+    department=StringField('Department',validators=[DataRequired()])
+    submit=SubmitField('submit')
